6e58a4f6f59eac602ba00cd08dbc953368f751eb
Kevin Chen <chen.kcw@gmail.com>
2021-12-01 01:13:35 -0800

    budget update action and refactor code

--- server/app/controllers/api/v1/budgets_controller.rb
+++ server/app/controllers/api/v1/budgets_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BudgetsController < Api::ApplicationController
+  before_action :budget_params, except: [:index]
 
   def index
     budgets = Budget.where(user: current_user)
@@ -6,17 +7,31 @@ class Api::V1::BudgetsController < Api::ApplicationController
   end
 
   def create
-    amount = params[:amount] * 100 # amount in cents
-    category = Category.find_by name: params[:category]
-    if !category
-      category = Category.create(name: params[:category])
-    end
-    budget = Budget.new(amount: amount, category: category)
+    budget = Budget.new(amount: @amount, category: @category)
     budget.user = current_user
     if budget.save
       render json: {id: budget.id}
     else
       render json: {errors: budget.errors, status: 422}
+    end
+  end
+
+  def update
+    budget = Budget.find params[:id]
+    if budget.update(amount: @amount, category: @category)
+      render json: {id: budget.id}
+    else
+      render json: { errors: budget.errors, status: 422}
+    end
+  end
+
+  private
+
+  def budget_params
+    @amount = params[:amount] * 100 # amount in cents
+    @category = Category.find_by name: params[:category]
+    if !@category
+      @category = Category.create(name: params[:category])
     end
   end
 end
--- server/app/models/budget.rb
+++ server/app/models/budget.rb
@@ -3,6 +3,7 @@ class Budget < ApplicationRecord
   belongs_to :category
 
   validates :amount, numericality: {greater_than: 0}
+  validates :category_id, uniqueness: { scope: :user_id, message: "has already been set" }
 
   def category_name
     c = Category.find(self.category_id)
