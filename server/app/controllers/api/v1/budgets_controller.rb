c646e9399b8f98a59f1119bad82b084004220824
Kevin Chen <chen.kcw@gmail.com>
2021-12-01 01:18:28 -0800

    budget destroy action

--- server/app/controllers/api/v1/budgets_controller.rb
+++ server/app/controllers/api/v1/budgets_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::BudgetsController < Api::ApplicationController
-  before_action :budget_params, except: [:index]
+  before_action :budget_params, except: [:index, :destroy]
+  before_action :find_budget, except: [:index, :create]
+  before_action :authenticate_user!
 
   def index
     budgets = Budget.where(user: current_user)
@@ -17,15 +19,26 @@ class Api::V1::BudgetsController < Api::ApplicationController
   end
 
   def update
-    budget = Budget.find params[:id]
-    if budget.update(amount: @amount, category: @category)
-      render json: {id: budget.id}
+    if @budget.update(amount: @amount, category: @category)
+      render json: {id: @budget.id}
     else
-      render json: { errors: budget.errors, status: 422}
+      render json: { errors: @budget.errors, status: 422}
+    end
+  end
+
+  def destroy
+    if @budget.destroy
+      render json: {status: 200}
+    else
+      render json: {status: 500}
     end
   end
 
   private
+
+  def find_budget
+    @budget = Budget.find params[:id]
+  end
 
   def budget_params
     @amount = params[:amount] * 100 # amount in cents
