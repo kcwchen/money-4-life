78d78cfb77fea7f41f6977be33b1ed499e17e40a
Kevin Chen <chen.kcw@gmail.com>
2021-12-08 02:24:10 -0800

    fix hero

--- landing/components/Hero.js
+++ landing/components/Hero.js
@@ -18,71 +18,66 @@ export default function Hero() {
   return (
     <Container
       maxW='100%'
-      // pos='relative'
+      pos='relative'
       pt={{ base: 0, md: 10 }}
-      backgroundImage={'./hero.svg'}
+      backgroundImage={{ base: 0, lg: './hero.svg' }}
       backgroundRepeat={'no-repeat'}
-      backgroundPositon={'right'}
-      backgroundSize={'contain contain'}
+      backgroundPosition={'right'}
+      backgroundSize={'contain'}
+      backgroundColor={'lightblue'}
     >
-      <Stack direction={'row'}>
-        <Stack
-          textAlign={'left'}
-          align={'flex-start'}
-          spacing={{ base: 8, md: 10 }}
-          pt={{ base: 20, md: 40 }}
-          pb={{ base: 20, md: 20 }}
-          ml={{ base: 20, md: 40 }}
+      <Stack
+        textAlign={'left'}
+        align='flex-start'
+        spacing={{ base: 8, md: 10 }}
+        pt={{ base: 20, md: 40 }}
+        pb={{ base: 20, md: 20 }}
+        ml={{ base: 20, md: 60 }}
+        maxW='lg'
+      >
+        <Heading fontWeight={600} fontSize='6xl' zIndex={5} data-aos='fade-up'>
+          Budgeting that works for you
+        </Heading>
+        <Text
+          fontSize='xl'
+          color={'gray.700'}
+          maxW={'3xl'}
+          zIndex={5}
+          data-aos='fade-up'
         >
-          <Heading
-            fontWeight={600}
-            fontSize='6xl'
-            zIndex={5}
+          Managing money can be hard. Don't do it alone. <br />
+          <strong>Money 4 Life</strong> empowers you to save more, spend less,
+          and take back control of your financial life.
+        </Text>
+        <Stack
+          spacing={6}
+          direction={'row'}
+          align='center'
+          pb={{ base: 8, md: 32 }}
+        >
+          <Button
+            data-aos='fade-up'
+            as='a'
+            href='http:/localhost:3001/signup'
+            size='lg'
+            rounded={'full'}
+            px={6}
+            colorScheme={'blue'}
+            bg={'#6F55FF'}
+            _hover={{ bg: '#5842d8' }}
+          >
+            Sign Up
+          </Button>
+          <Button
+            as='a'
+            size='lg'
+            href='#features'
+            rounded={'full'}
+            px={6}
             data-aos='fade-up'
           >
-            Budgeting that works for you
-          </Heading>
-          <Text
-            fontSize='xl'
-            color={'gray.700'}
-            maxW={'3xl'}
-            zIndex={5}
-            data-aos='fade-up'
-          >
-            Managing money can be hard. Don't do it alone.{' '}
-            <strong>Money 4 Life</strong> empowers you to save more, spend less,
-            and take back control of your financial life.
-          </Text>
-          <Stack
-            spacing={6}
-            direction={'row'}
-            align='center'
-            pb={{ base: 8, md: 32 }}
-          >
-            <Button
-              data-aos='fade-up'
-              as='a'
-              href='http:/localhost:3001/signup'
-              size='lg'
-              rounded={'full'}
-              px={6}
-              colorScheme={'blue'}
-              bg={'#6F55FF'}
-              _hover={{ bg: '#5842d8' }}
-            >
-              Sign Up
-            </Button>
-            <Button
-              as='a'
-              size='lg'
-              href='#features'
-              rounded={'full'}
-              px={6}
-              data-aos='fade-up'
-            >
-              Learn more
-            </Button>
-          </Stack>
+            Learn more
+          </Button>
         </Stack>
       </Stack>
     </Container>
--- landing/pages/index.js
+++ landing/pages/index.js
@@ -1,11 +1,16 @@
 import WelcomePage from '../components/WelcomePage';
 import NavBar from '../components/NavBar';
+import Head from 'next/head';
 import Hero from '../components/Hero';
 import Nav from '../components/Nav.tsx';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Money 4 Life | Keep track of your spending habits</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Nav />
       <Hero />
     </>
